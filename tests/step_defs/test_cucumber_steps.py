24f5c0484b0c6db533ec4fea991e41f7ba9d6b2b
Julio Briones <Julio_Briones@epam.com>
2021-12-29 10:26:52 -0600

    Refactored code with partial function.

--- tests/step_defs/test_cucumber_steps.py
+++ tests/step_defs/test_cucumber_steps.py
@@ -28,3 +28,33 @@ def remove_cucumbers(basket, some):
 @then(partial_num('the basket contains "{total:Number}" cucumbers'))
 def basket_has_total(basket, total):
     assert basket.count == total
+
+
+@given('the basket has "8" cucumbers')
+def step_impl():
+    raise NotImplementedError(u'STEP: Given the basket has "8" cucumbers')
+
+
+@given('the basket has "2" cucumbers')
+def step_impl():
+    raise NotImplementedError(u'STEP: Given the basket has "2" cucumbers')
+
+
+@when('"4" cucumbers are added to the basket')
+def step_impl():
+    raise NotImplementedError(u'STEP: When "4" cucumbers are added to the basket')
+
+
+@then('the basket contains "6" cucumbers')
+def step_impl():
+    raise NotImplementedError(u'STEP: Then the basket contains "6" cucumbers')
+
+
+@when('"3" cucumbers are removed from the basket')
+def step_impl():
+    raise NotImplementedError(u'STEP: When "3" cucumbers are removed from the basket')
+
+
+@then('the basket contains "5" cucumbers')
+def step_impl():
+    raise NotImplementedError(u'STEP: Then the basket contains "5" cucumbers')
