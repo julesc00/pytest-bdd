354eaff24599f1a1b1b5f094fb5857151abd8cb5
Julio Briones <Julio_Briones@epam.com>
2021-12-29 08:58:25 -0600

    Finished chapter/section 4.

--- tests/step_defs/test_cucumber_steps.py
+++ tests/step_defs/test_cucumber_steps.py
@@ -1,23 +1,30 @@
-from pytest_bdd import scenario, given, when, then
-
+from functools import partial
+from pytest_bdd import scenarios, parsers, given, when, then
 from cucumbers import CucumberBasket
 
-
-@scenario("../features/cucumbers.feature", "Add cucumbers to a basket")
-def test_add():
-    pass
-
-
-@given("the basket has 2 cucumbers", target_fixture="basket")
-def basket():
-    return CucumberBasket(initial_count=2)
+EXTRA_TYPES = {
+    "Number": int,
+}
+# helper function to avoid adding the test_task() function
+scenarios("../features/cucumbers.feature")
+partial_num = partial(parsers.cfparse, extra_types=EXTRA_TYPES)
 
 
-@when("4 cucumbers are added to the basket")
-def add_cucumbers(basket):
-    basket.add(4)
+@given(partial_num('the basket has "{initial:Number}" cucumbers'), target_fixture='basket')
+def basket(initial):
+    return CucumberBasket(initial_count=initial)
 
 
-@then("the basket contains 6 cucumbers")
-def basket_has_total(basket):
-    assert basket.count == 6
+@when(partial_num('"{some:Number}" cucumbers are added to the basket'))
+def add_cucumbers(basket, some):
+    basket.add(some)
+
+
+@when(partial_num('"{some:Number}" cucumbers are removed from the basket'))
+def remove_cucumbers(basket, some):
+    basket.remove(some)
+
+
+@then(partial_num('the basket contains "{total:Number}" cucumbers'))
+def basket_has_total(basket, total):
+    assert basket.count == total
